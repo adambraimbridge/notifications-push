1170fe8a7bac916352ed5dbbc8ca246d39ae4636
Mihai Bulica <mihai.bulica@ft.com>
2018-09-12 15:57:52 +0300

    fixed tests

--- consumer/consume_test.go
+++ consumer/consume_test.go
@@ -118,7 +118,7 @@ func TestAcceptNotificationBasedOnAudioContentType(t *testing.T) {
 
 	handler := NewMessageQueueHandler(defaultContentUriWhitelist, contentTypeWhitelist, mapper, dispatcher)
 
-	msg := kafka.NewFTMessage(map[string]string{"X-Request-Id": "tid_summin", "ContentType": "application/vnd.ft-upp-audio+json"},
+	msg := kafka.NewFTMessage(map[string]string{"X-Request-Id": "tid_summin", "Content-Type": "application/vnd.ft-upp-audio+json"},
 	`{"ContentURI": "http://not-in-the-whitelist.svc.ft.com:8080/lists/blah/55e40823-6804-4264-ac2f-b29e11bf756a"}`)
 
 	err := handler.HandleMessage(msg)
